4d24e78883776841b0037bef24855de82295394c
Hitoshi Otsuki <hitochan777@gmail.com>
2017-06-04 22:01:09 +0900

    Add schedule to cron job converter

--- text_parser.go
+++ text_parser.go
@@ -4,6 +4,9 @@ import (
 	"reflect"
 	"strconv"
 	"log"
+	"github.com/jasonlvhit/gocron"
+	"time"
+	"fmt"
 )
 
 type Schedule struct {
@@ -31,6 +34,50 @@ const (
 
 func (sched1 *Schedule) Compare(sched2 *Schedule) bool {
 	return reflect.DeepEqual(*sched1, *sched2)
+}
+
+func (sched *Schedule) GetCronJobs() []*gocron.Job {
+	var jobs []*gocron.Job
+	for _, day := range sched.Day {
+		for _, time := range sched.Time {
+			jobs = append(jobs, getCronJob(day, time))
+		}
+	}
+	return jobs
+}
+
+func getCronJob(d int, t int) *gocron.Job {
+	var job *gocron.Job
+	switch d {
+		case Sunday:
+			job = gocron.Every(1).Saturday()
+			break
+		case Monday:
+			job = gocron.Every(1).Monday()
+			break
+		case Tuesday:
+			job = gocron.Every(1).Tuesday()
+			break
+		case Wednesday:
+			job = gocron.Every(1).Wednesday()
+			break
+		case Thursday:
+			job = gocron.Every(1).Thursday()
+			break
+		case Friday:
+			job = gocron.Every(1).Friday()
+			break
+		case Saturday:
+			job = gocron.Every(1).Saturday()
+			break
+		default:
+			return nil
+	}
+
+	duration := time.Duration(t)
+	timeString := fmt.Sprintf("%d:%d", duration.Hours(), duration.Minutes())
+	job = job.At(timeString)
+	return job
 }
 
 func NewSchedule(time []int, day []int) *Schedule {
