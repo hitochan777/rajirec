abe74d219ef154ab3eb67369cddc6f565064b102
Hitoshi Otsuki <hitochan777@gmail.com>
2017-06-07 20:27:46 +0900

    Refactoring and add Map

--- utils.go
+++ utils.go
@@ -1,5 +1,13 @@
 package rajirec
 
+import (
+	"log"
+	"errors"
+	"net/http"
+	"io/ioutil"
+	"encoding/xml"
+)
+
 func AppendAllIfMissing(slice []int, i []int) []int {
 	for _, ele := range i {
 		slice = AppendIfMissing(slice, ele)
@@ -14,4 +22,43 @@ func AppendIfMissing(slice []int, i int) []int {
 		}
 	}
 	return append(slice, i)
+}
+
+type Map struct {
+	children map[string]*Map
+	value interface{}
+}
+
+func (m *Map) Get(key string) *Map {
+	val, ok := m.children[key]
+	if ok {
+		if val == nil {
+			return nil
+		} else {
+			return val
+		}
+	} else {
+		m.children[key] = &Map{}
+		return m.children[key]
+	}
+}
+
+func (m *Map) Set(value interface{}, keys ...string) {
+	m1 := m
+	for _, key := range keys {
+		m1 = m.Get(key)
+	}
+	m1.value = value
+}
+
+func FetchXML(url string, v interface{}) {
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		log.Fatal("Failed to fetch XML data")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err := xml.Unmarshal(body, v); err != nil {
+		log.Fatal(err)
+	}
 }
