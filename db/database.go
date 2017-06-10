9c350dd7b4aa56d2be0cb5e54729f7285981d651
Hitoshi Otsuki <hitochan777@gmail.com>
2017-06-10 19:07:36 +0900

    Add db

---
+++ db/database.go
@@ -0,0 +1,64 @@
+package db
+
+import (
+    "github.com/nanobox-io/golang-scribble"
+
+	"github.com/hitochan777/rajirec/schedule"
+)
+
+type DBManager struct {
+	collection string
+	resource string
+	db *scribble.Driver
+}
+
+func NewDBManager(dir, collection, resource string) (*DBManager, error) {
+	db, err := scribble.New(dir, nil)
+	dbManager := &DBManager{collection, resource,db}
+	return dbManager, err
+}
+
+func (dbm *DBManager) SaveSchedule(sched schedule.Schedule) error {
+	schedules := dbm.GetSchedules()
+	schedules = append(schedules, sched)
+	if err := dbm.db.Write(dbm.collection, dbm.resource, schedules); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (dbm *DBManager) GetSchedules() []schedule.Schedule {
+	var scheds []schedule.Schedule
+	if err := dbm.db.Read(dbm.collection, dbm.resource, &scheds); err != nil {
+		return []schedule.Schedule{}
+	}
+
+	return scheds
+}
+
+/*func (dbm *DBManager) SaveRecord(record Record) error {
+	records := dbm.GetSchedules()
+	records = append(records, record)
+	if err := dbm.db.Write(dbm.collection, dbm.resource, records); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (dbm *DBManager) GetRecords() []Record {
+	var records interface{}
+	if err := dbm.db.Read(dbm.collection, dbm.resource, &records); err != nil {
+		return []Record{}
+	}
+
+	return []Record{}
+}*/
+
+func (dbm *DBManager) GetCollection() string {
+	return dbm.collection
+}
+
+func (dbm *DBManager) GetResource() string {
+	return dbm.resource
+}
+
